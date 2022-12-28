cf8081f80805d27a2144c389c0dfedfa36dafea6
mpayson <mpayson@esri.com>
2022-12-27 20:44:13 -0500

    tile select issue

--- react/src/App.js
+++ react/src/App.js
@@ -1,34 +1,32 @@
-import React, { useState } from "react";
-import "@esri/calcite-components/dist/components/calcite-button";
-import "@esri/calcite-components/dist/components/calcite-icon";
-import "@esri/calcite-components/dist/components/calcite-slider";
-import {
-  CalciteButton,
-  CalciteIcon,
-  CalciteSlider,
-} from "@esri/calcite-components-react";
+import React from "react";
+import "@esri/calcite-components/dist/components/calcite-modal";
+import "@esri/calcite-components/dist/components/calcite-tile";
+import "@esri/calcite-components/dist/components/calcite-tile-select";
+import "@esri/calcite-components/dist/components/calcite-tile-select-group";
+import "@esri/calcite-components/dist/components/calcite-radio-button";
+
 import "@esri/calcite-components/dist/calcite/calcite.css";
 
 function App() {
-  const [sliderValue, setSliderValue] = useState(50);
-
   return (
-    <>
-      <h1>
-        Hello, React <CalciteIcon icon="banana" />
-      </h1>
-      <CalciteButton onClick={(e) => setSliderValue(0)}>
-        Reset
-      </CalciteButton>
-      <CalciteSlider
-        min="1"
-        max="100"
-        value={sliderValue}
-        step="1"
-        onCalciteSliderUpdate={(e) => setSliderValue(e.target.value)}
-      />
-      <p>The slider currently has a value of {sliderValue}</p>
-    </>
+    <calcite-tile-select-group>
+      <calcite-tile-select
+        heading="Option 1"
+        name="cloud-selection"
+        input-enabled
+        width="full"
+        type="radio"
+        value="1"
+      ></calcite-tile-select>
+      <calcite-tile-select
+        heading="Option 2"
+        name="cloud-selection"
+        input-enabled
+        width="full"
+        type="radio"
+        value="2"
+      ></calcite-tile-select>
+    </calcite-tile-select-group>
   );
 }
 
